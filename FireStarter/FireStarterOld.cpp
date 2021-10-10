47e936bf7c1cca89fd3a027429217ea4db88ec62
GrangerFX <grangerfx@gmail.com>
2021-10-10 11:49:33 -0700

    Disabled the self modifying code as the random program generation and separate add and multiply ocodes are interesting.

--- FireStarter/FireStarterOld.cpp
+++ FireStarter/FireStarterOld.cpp
@@ -432,7 +432,7 @@ void FireStarter::MakeProgram(std::string &src)
         "    for (int p = 0; p < PROGRAM_ITERATIONS; p++) {\n"
         "        float curResult = fabsf(Evaluate(data, 0.0f) - Target(0.0f));\n"
         "        for (int i = 1; i < SAMPLE_ITERATIONS; i++) {\n"
-#if 1
+#if 0
         "            float delta = fabsf(Execute(program, data, theta[i]) - target[i]);\n"
 #else
         "            float delta = fabsf(Evaluate(data, theta[i]) - target[i]);\n"
