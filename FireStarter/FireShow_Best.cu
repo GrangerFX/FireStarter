dca8fd9beb0938d350b6a00e41109bc82259f061
GrangerFX <grangerfx@gmail.com>
2022-01-09 19:59:04 -0800

    Three stage evolution first light.

--- FireStarter/FireShow_Best.cu
+++ FireStarter/FireShow_Best.cu
@@ -19,26 +19,26 @@ inline float Target(float n, unsigned int variation)
 GPU_FUNCTION float InitData0(FireStarterData &data)
 {
 // DATA0 //
-    data.d[0] = -0.050911f;
-    data.d[1] = -0.095168f;
-    data.d[2] = -55.678799f;
-    data.d[3] = -15.918847f;
-    data.d[4] = -0.756818f;
-    data.d[5] = -0.115125f;
-    data.d[6] = 0.021131f;
-    data.d[7] = -0.424197f;
-    data.d[8] = 1.633703f;
-    data.d[9] = -0.234050f;
-    data.d[10] = -2.232526f;
-    data.d[11] = -2.143106f;
-    data.d[12] = -1.498720f;
-    data.d[13] = -0.654248f;
-    data.d[14] = 0.578747f;
-    data.d[15] = -1.296861f;
-    data.d[16] = 3.129124f;
-    data.d[17] = 0.365554f;
-    data.d[18] = 8.608454f;
-    data.d[19] = -0.048320f;
+    data.d[0] = 0.625101f;
+    data.d[1] = -1.963810f;
+    data.d[2] = 0.132740f;
+    data.d[3] = -0.132741f;
+    data.d[4] = 0.068899f;
+    data.d[5] = -0.020627f;
+    data.d[6] = 6.372983f;
+    data.d[7] = -4.410119f;
+    data.d[8] = -6.375785f;
+    data.d[9] = 7.536968f;
+    data.d[10] = 2.627861f;
+    data.d[11] = -17.460800f;
+    data.d[12] = 22.221685f;
+    data.d[13] = 15.905842f;
+    data.d[14] = -8.541908f;
+    data.d[15] = -0.000001f;
+    data.d[16] = -4.158216f;
+    data.d[17] = 1.503352f;
+    data.d[18] = 0.000006f;
+    data.d[19] = 0.000000f;
     data.d[20] = 0.000000f;
     data.d[21] = 0.000000f;
     data.d[22] = 0.000000f;
@@ -51,33 +51,33 @@ GPU_FUNCTION float InitData0(FireStarterData &data)
     data.d[29] = 0.000000f;
     data.d[30] = 0.000000f;
     data.d[31] = 0.000000f;
-    return 0.000026;
+    return 0.000000;
 // END //
 } // InitData0
 
 GPU_FUNCTION float InitData1(FireStarterData &data)
 {
 // DATA1 //
-    data.d[0] = -0.057789f;
-    data.d[1] = -0.051612f;
-    data.d[2] = -58.158268f;
-    data.d[3] = -12.618521f;
-    data.d[4] = -0.094677f;
-    data.d[5] = -0.031466f;
-    data.d[6] = 0.108511f;
-    data.d[7] = 0.265973f;
-    data.d[8] = 1.599992f;
-    data.d[9] = -0.224752f;
-    data.d[10] = -2.208896f;
-    data.d[11] = -2.204640f;
-    data.d[12] = -1.389546f;
-    data.d[13] = -0.770473f;
-    data.d[14] = 0.521914f;
-    data.d[15] = -0.335802f;
-    data.d[16] = 3.250836f;
-    data.d[17] = 0.730017f;
-    data.d[18] = 10.149578f;
-    data.d[19] = -0.029820f;
+    data.d[0] = 0.609682f;
+    data.d[1] = -1.884315f;
+    data.d[2] = 0.138154f;
+    data.d[3] = -0.138156f;
+    data.d[4] = 0.056093f;
+    data.d[5] = -0.000328f;
+    data.d[6] = 5.368011f;
+    data.d[7] = -4.413926f;
+    data.d[8] = -6.001370f;
+    data.d[9] = 7.266527f;
+    data.d[10] = 2.255331f;
+    data.d[11] = -17.035461f;
+    data.d[12] = 20.018755f;
+    data.d[13] = 13.055929f;
+    data.d[14] = -10.803656f;
+    data.d[15] = -0.000006f;
+    data.d[16] = -4.150561f;
+    data.d[17] = 2.301107f;
+    data.d[18] = -0.118124f;
+    data.d[19] = 0.000000f;
     data.d[20] = 0.000000f;
     data.d[21] = 0.000000f;
     data.d[22] = 0.000000f;
@@ -90,7 +90,7 @@ GPU_FUNCTION float InitData1(FireStarterData &data)
     data.d[29] = 0.000000f;
     data.d[30] = 0.000000f;
     data.d[31] = 0.000000f;
-    return 0.000021;
+    return 0.000000;
 // END //
 } // InitData1
 
@@ -101,34 +101,34 @@ GPU_FUNCTION float Evaluate(FireStarterData data, float n)
     n = data.d[1] += n;
     n = data.d[2] *= n;
     n = data.d[3] += n;
-    n = data.d[4] *= n;
+    n = data.d[1] *= n;
     n = data.d[3] += n;
+    n = data.d[4] *= n;
+    n = data.d[5] += n;
+    n = data.d[6] *= n;
+    n = data.d[7] += n;
     n = data.d[5] *= n;
-    n = data.d[6] += n;
-    n = data.d[7] *= n;
-    n = data.d[8] += n;
-    n = data.d[9] *= n;
-    n = data.d[6] += n;
+    n = data.d[5] += n;
+    n = data.d[8] *= n;
+    n = data.d[9] += n;
     n = data.d[10] *= n;
     n = data.d[11] += n;
     n = data.d[12] *= n;
-    n = data.d[13] += n;
-    n = data.d[10] *= n;
-    n = data.d[8] += n;
-    n = data.d[14] *= n;
-    n = data.d[13] += n;
-    n = data.d[10] *= n;
-    n = data.d[15] += n;
-    n = data.d[16] *= n;
-    n = data.d[17] += n;
-    n = data.d[6] *= n;
+    n = data.d[10] += n;
+    n = data.d[11] *= n;
     n = data.d[12] += n;
     n = data.d[13] *= n;
-    n = data.d[18] += n;
-    n = data.d[19] *= n;
     n = data.d[14] += n;
-    n = data.d[12] *= n;
-    n = data.d[1] += n;
+    n = data.d[15] *= n;
+    n = data.d[7] += n;
+    n = data.d[5] *= n;
+    n = data.d[16] += n;
+    n = data.d[3] *= n;
+    n = data.d[17] += n;
+    n = data.d[16] *= n;
+    n = data.d[4] += n;
+    n = data.d[2] *= n;
+    n = data.d[18] += n;
 // END //
     return isnan(n) ? 0.0f : n;
 } // Evaluate
--- FireStarter/FireStarter_Solution.h
+++ FireStarter/FireStarter_Solution.h
@@ -1,8 +1,8 @@
 #include <math.h>
 
-// Run date: 01/09/22 19:45:39 Pacific Standard Time
-// Run duration = 0.403285 seconds
-// Run count = 26
+// Run date: 01/09/22 19:58:06 Pacific Standard Time
+// Run duration = 0.433981 seconds
+// Run count = 48
 // Run units = 1
 // Run population = 4352
 // Run iterations = 15
@@ -24,118 +24,116 @@ inline float Target(float n, unsigned int variation)
     }
 } // Target
 
-// Solution0 precision = 0.000026
+// Solution0 precision = 0.000000
 inline float Solution0(float n)
 {
-    float d0 = -0.050911f;
-    float d1 = -0.095168f;
-    float d2 = -55.678799f;
-    float d3 = -15.918847f;
-    float d4 = -0.756818f;
-    float d5 = -0.115125f;
-    float d6 = 0.021131f;
-    float d7 = -0.424197f;
-    float d8 = 1.633703f;
-    float d9 = -0.234050f;
-    float d10 = -2.232526f;
-    float d11 = -2.143106f;
-    float d12 = -1.498720f;
-    float d13 = -0.654248f;
-    float d14 = 0.578747f;
-    float d15 = -1.296861f;
-    float d16 = 3.129124f;
-    float d17 = 0.365554f;
-    float d18 = 8.608454f;
-    float d19 = -0.048320f;
+    float d0 = 0.625101f;
+    float d1 = -1.963810f;
+    float d2 = 0.132740f;
+    float d3 = -0.132741f;
+    float d4 = 0.068899f;
+    float d5 = -0.020627f;
+    float d6 = 6.372983f;
+    float d7 = -4.410119f;
+    float d8 = -6.375785f;
+    float d9 = 7.536968f;
+    float d10 = 2.627861f;
+    float d11 = -17.460800f;
+    float d12 = 22.221685f;
+    float d13 = 15.905842f;
+    float d14 = -8.541908f;
+    float d15 = -0.000001f;
+    float d16 = -4.158216f;
+    float d17 = 1.503352f;
+    float d18 = 0.000006f;
     n = d0 *= n;
     n = d1 += n;
     n = d2 *= n;
     n = d3 += n;
-    n = d4 *= n;
+    n = d1 *= n;
     n = d3 += n;
+    n = d4 *= n;
+    n = d5 += n;
+    n = d6 *= n;
+    n = d7 += n;
     n = d5 *= n;
-    n = d6 += n;
-    n = d7 *= n;
-    n = d8 += n;
-    n = d9 *= n;
-    n = d6 += n;
+    n = d5 += n;
+    n = d8 *= n;
+    n = d9 += n;
     n = d10 *= n;
     n = d11 += n;
     n = d12 *= n;
-    n = d13 += n;
-    n = d10 *= n;
-    n = d8 += n;
-    n = d14 *= n;
-    n = d13 += n;
-    n = d10 *= n;
-    n = d15 += n;
-    n = d16 *= n;
-    n = d17 += n;
-    n = d6 *= n;
+    n = d10 += n;
+    n = d11 *= n;
     n = d12 += n;
     n = d13 *= n;
-    n = d18 += n;
-    n = d19 *= n;
     n = d14 += n;
-    n = d12 *= n;
-    n = d1 += n;
+    n = d15 *= n;
+    n = d7 += n;
+    n = d5 *= n;
+    n = d16 += n;
+    n = d3 *= n;
+    n = d17 += n;
+    n = d16 *= n;
+    n = d4 += n;
+    n = d2 *= n;
+    n = d18 += n;
     return n;
 } // Solution0
 
-// Solution1 precision = 0.000021
+// Solution1 precision = 0.000000
 inline float Solution1(float n)
 {
-    float d0 = -0.057789f;
-    float d1 = -0.051612f;
-    float d2 = -58.158268f;
-    float d3 = -12.618521f;
-    float d4 = -0.094677f;
-    float d5 = -0.031466f;
-    float d6 = 0.108511f;
-    float d7 = 0.265973f;
-    float d8 = 1.599992f;
-    float d9 = -0.224752f;
-    float d10 = -2.208896f;
-    float d11 = -2.204640f;
-    float d12 = -1.389546f;
-    float d13 = -0.770473f;
-    float d14 = 0.521914f;
-    float d15 = -0.335802f;
-    float d16 = 3.250836f;
-    float d17 = 0.730017f;
-    float d18 = 10.149578f;
-    float d19 = -0.029820f;
+    float d0 = 0.609682f;
+    float d1 = -1.884315f;
+    float d2 = 0.138154f;
+    float d3 = -0.138156f;
+    float d4 = 0.056093f;
+    float d5 = -0.000328f;
+    float d6 = 5.368011f;
+    float d7 = -4.413926f;
+    float d8 = -6.001370f;
+    float d9 = 7.266527f;
+    float d10 = 2.255331f;
+    float d11 = -17.035461f;
+    float d12 = 20.018755f;
+    float d13 = 13.055929f;
+    float d14 = -10.803656f;
+    float d15 = -0.000006f;
+    float d16 = -4.150561f;
+    float d17 = 2.301107f;
+    float d18 = -0.118124f;
     n = d0 *= n;
     n = d1 += n;
     n = d2 *= n;
     n = d3 += n;
-    n = d4 *= n;
+    n = d1 *= n;
     n = d3 += n;
+    n = d4 *= n;
+    n = d5 += n;
+    n = d6 *= n;
+    n = d7 += n;
     n = d5 *= n;
-    n = d6 += n;
-    n = d7 *= n;
-    n = d8 += n;
-    n = d9 *= n;
-    n = d6 += n;
+    n = d5 += n;
+    n = d8 *= n;
+    n = d9 += n;
     n = d10 *= n;
     n = d11 += n;
     n = d12 *= n;
-    n = d13 += n;
-    n = d10 *= n;
-    n = d8 += n;
-    n = d14 *= n;
-    n = d13 += n;
-    n = d10 *= n;
-    n = d15 += n;
-    n = d16 *= n;
-    n = d17 += n;
-    n = d6 *= n;
+    n = d10 += n;
+    n = d11 *= n;
     n = d12 += n;
     n = d13 *= n;
-    n = d18 += n;
-    n = d19 *= n;
     n = d14 += n;
-    n = d12 *= n;
-    n = d1 += n;
+    n = d15 *= n;
+    n = d7 += n;
+    n = d5 *= n;
+    n = d16 += n;
+    n = d3 *= n;
+    n = d17 += n;
+    n = d16 *= n;
+    n = d4 += n;
+    n = d2 *= n;
+    n = d18 += n;
     return n;
 } // Solution1
