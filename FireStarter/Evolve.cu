0250f19b11ac1324a9bee7c48de0c008408203b4
GrangerFX <grangerfx@gmail.com>
2022-02-20 12:40:00 -0800

    Early termination of bad evolutions.

--- FireStarter/Evolve.cu
+++ FireStarter/Evolve.cu
@@ -16,51 +16,42 @@ GPU_GLOBAL void Evolve(FireStarterResults* newResults, FireStarterResults* oldRe
 
     GPU_SHARED FireStarterInstructions instructions;
     float oldResult;
-    if (thread == 0)
-        if (!generation) {
-            // The first generation's instructions are random.
-            for (int i = 0; i < PROGRAM_INSTRUCTIONS; i++)
-                instructions.i[i].Random(i, memberSeed);
-            oldResult = START_RESULT;
-        } else {
-            // Later generations randomize one instruction.
-            instructions = oldResults->results[member].instructions;
-            unsigned int i = RANDOMSEED(memberSeed) % PROGRAM_INSTRUCTIONS;
+    if (!generation) {
+        // The first generation's instructions are random.
+        for (int i = 0; i < PROGRAM_INSTRUCTIONS; i++)
             instructions.i[i].Random(i, memberSeed);
-            oldResult = oldResults->results[member].maxResult;
-        }
-    GPU_SYNCTHREADS();
-
-    float theta[SAMPLE_ITERATIONS];
-    float sampleStep = (SAMPLE_MAX - SAMPLE_MIN) / (SAMPLE_ITERATIONS - 1);
-    for (int i = 0; i < SAMPLE_ITERATIONS; i++)
-        theta[i] = SAMPLE_MIN + i * sampleStep;
+        oldResult = START_RESULT;
+    } else {
+        // Later generations randomize one instruction.
+        instructions = oldResults->results[member].instructions;
+        unsigned int i = RANDOMSEED(memberSeed) % PROGRAM_INSTRUCTIONS;
+        instructions.i[i].Random(i, memberSeed);
+        oldResult = oldResults->results[member].maxResult;
+    }
 
     // Evolve the program data for each variation.
     GPU_SHARED FireStarterData threadData[EVOLVE_THREADS];
     FireStarterData& data = threadData[thread];
     float maxResult = 0.0f;
     for (unsigned int v = 0; v < TARGET_VARIATIONS; v++) {
-        float target[SAMPLE_ITERATIONS];
-        for (int i = 0; i < SAMPLE_ITERATIONS; i++)
-            target[i] = Target(theta[i], v);
-        float lastResult;
-        for (int i = 0; i < PROGRAM_INSTRUCTIONS; i++) {
+        for (int i = 0; i < PROGRAM_INSTRUCTIONS; i++)
             data.d[i] = RANDOMFACTOR(threadSeed);
-            lastResult = START_RESULT;
-        }
-        float result = lastResult;
-        for (unsigned int p = 0; p < iterations; p++) {
-            unsigned int d = RANDOMSEED(threadSeed) % PROGRAM_INSTRUCTIONS;
-            float oldData = data.d[d];
-            data.d[d] = oldData + (EVOLUTION_FACTOR * RANDOMFACTOR(threadSeed) * result);
-            float curResult = 0.0f;
-            for (int i = 0; i < SAMPLE_ITERATIONS; i++)
-                curResult = fmaxf(fabsf(instructions.Execute(data, theta[i]) - target[i]), curResult);
-            if (curResult < result)
-                result = curResult;
-            else
-                data.d[d] = oldData;
+        float result = START_RESULT;
+        if (maxResult <= oldResult) {
+            for (unsigned int p = 0; p < iterations; p++) {
+                unsigned int d = RANDOMSEED(threadSeed) % PROGRAM_INSTRUCTIONS;
+                const float oldData = data.d[d];
+                data.d[d] = oldData + (EVOLUTION_FACTOR * RANDOMFACTOR(threadSeed) * result);
+                float curResult = 0.0f;
+                for (int i = 0; i < SAMPLE_ITERATIONS; i++) {
+                    const float theta = SAMPLE_MIN + i * ((SAMPLE_MAX - SAMPLE_MIN) / (SAMPLE_ITERATIONS - 1));
+                    curResult = fmaxf(fabsf(instructions.Execute(data, theta) - Target(theta, v)), curResult);
+                }
+                if (curResult < result)
+                    result = curResult;
+                else
+                    data.d[d] = oldData;
+            }
         }
 
         // Find the best result among all the warp threads.
@@ -75,13 +66,11 @@ GPU_GLOBAL void Evolve(FireStarterResults* newResults, FireStarterResults* oldRe
                 minResult = threadResults[i];
             }
         }
-        if ((thread == minIndex) && (!generation || (minResult < lastResult))) {
+        if (thread == minIndex) {
             newResults->results[member].data[v] = data;
             newResults->results[member].minResult[v] = minResult;
         }
         maxResult = fmaxf(maxResult, minResult);
-//        if (generation && (maxResult > oldResult))
-//            break;
     }
 
     // Only read and write memory in a single thread.
