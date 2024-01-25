4acdd6558c663bc2351eb465cd83be659a12640e
Mark Granger <grangerfx@gmail.com>
2024-01-25 10:01:05 -0800

    256 tests of current evolution algorithm: 6 good.

---
+++ Distribution/Logs/2024-01-24_18-38-26_FireStarter_BestCode.h
@@ -0,0 +1,317 @@
+#pragma once
+
+#include "HashRandom.h"
+#include "FireStarterResults.h"
+#include "CUDADefines.h"
+
+// EVALUATE //
+// Run date: 01/24/24 18:38:26 Pacific Standard Time
+// Run duration = 28292.064899 seconds
+// Run generation = 85
+// Run evolution = 11
+// Run max result = 0.00003541
+// Run variations = 3
+// Run samples = 15
+// Run instructions = 32
+// Run registers = 29
+// Run opcodes = 2
+// Run patternOpcodes = 4
+
+// Run targetMin = 0.000000f
+// Run targetMax = 6.283185f
+// Run evolveTarget = 0.000001f
+
+// Run mode = FIRESTARTER_EVOLVE
+// Run evolveSeed = 0
+// Run optimizeSeed = 0
+// Run tests = 256
+// Run streams = 8
+// Run units = 1
+// Run states = 1
+// Run generations = 128
+// Run population = 278528
+// Run iterations = 64
+// Run passes = 500
+// Run precision = 0
+// Run candidates = 16
+// Run attempts = 0
+// Run optimize = 8
+
+// Run scale = 0.100000f
+// Run startScale = 2.000000f
+// Run startResult = 10.000000f
+
+// Variation: 0  result = 0.00000432
+inline void LoadVariation0(FireStarterResult* result)
+{
+    FireStarterData *data = result->Data();
+    data->d[0] = 1.118950f;
+    data->d[1] = 0.580150f;
+    data->d[2] = 0.163478f;
+    data->d[3] = -1.811663f;
+    data->d[4] = 0.332659f;
+    data->d[5] = 3.635553f;
+    data->d[6] = -0.072048f;
+    data->d[7] = -2.591888f;
+    data->d[8] = 0.007101f;
+    data->d[9] = -1.229581f;
+    data->d[10] = 1.521189f;
+    data->d[11] = -1.091278f;
+    data->d[12] = -0.107174f;
+    data->d[13] = 0.730101f;
+    data->d[14] = 0.648891f;
+    data->d[15] = -0.697794f;
+    data->d[16] = -0.207229f;
+    data->d[17] = -18.112320f;
+    data->d[18] = 2.318838f;
+    data->d[19] = 0.000000f;
+    data->d[20] = 0.000000f;
+    data->d[21] = 0.000000f;
+    data->d[22] = 0.000000f;
+    data->d[23] = 0.000000f;
+    data->d[24] = 0.000000f;
+    data->d[25] = 0.000000f;
+    data->d[26] = 0.000000f;
+    data->d[27] = 0.000000f;
+    data->d[28] = 0.000000f;
+    *(result->MinResult()) = 0.000004f;
+} // LoadVariation0
+
+// Variation: 1  result = 0.00001127
+inline void LoadVariation1(FireStarterResult* result)
+{
+    FireStarterData *data = result->Data();
+    data->d[0] = 0.628711f;
+    data->d[1] = -0.217259f;
+    data->d[2] = 0.657857f;
+    data->d[3] = 0.501201f;
+    data->d[4] = 0.830720f;
+    data->d[5] = -0.430050f;
+    data->d[6] = 0.194713f;
+    data->d[7] = 99.121277f;
+    data->d[8] = 2.792198f;
+    data->d[9] = 0.010259f;
+    data->d[10] = -2.457525f;
+    data->d[11] = 1.620431f;
+    data->d[12] = -1.512001f;
+    data->d[13] = -0.874730f;
+    data->d[14] = -1.288462f;
+    data->d[15] = -1.140415f;
+    data->d[16] = -0.621398f;
+    data->d[17] = 0.155819f;
+    data->d[18] = 0.898891f;
+    data->d[19] = 0.000000f;
+    data->d[20] = 0.000000f;
+    data->d[21] = 0.000000f;
+    data->d[22] = 0.000000f;
+    data->d[23] = 0.000000f;
+    data->d[24] = 0.000000f;
+    data->d[25] = 0.000000f;
+    data->d[26] = 0.000000f;
+    data->d[27] = 0.000000f;
+    data->d[28] = 0.000000f;
+    *(result->MinResult()) = 0.000011f;
+} // LoadVariation1
+
+// Variation: 2  result = 0.00003541
+inline void LoadVariation2(FireStarterResult* result)
+{
+    FireStarterData *data = result->Data();
+    data->d[0] = 0.627451f;
+    data->d[1] = 0.331815f;
+    data->d[2] = -1.047511f;
+    data->d[3] = 0.688367f;
+    data->d[4] = 1.840872f;
+    data->d[5] = -0.893989f;
+    data->d[6] = 0.148853f;
+    data->d[7] = 1.974620f;
+    data->d[8] = -0.041101f;
+    data->d[9] = 17.465673f;
+    data->d[10] = -7.944679f;
+    data->d[11] = 3.962412f;
+    data->d[12] = -0.277076f;
+    data->d[13] = 0.525647f;
+    data->d[14] = -1.639064f;
+    data->d[15] = -2.509681f;
+    data->d[16] = 1.648768f;
+    data->d[17] = -0.041141f;
+    data->d[18] = 1.503695f;
+    data->d[19] = 0.000000f;
+    data->d[20] = 0.000000f;
+    data->d[21] = 0.000000f;
+    data->d[22] = 0.000000f;
+    data->d[23] = 0.000000f;
+    data->d[24] = 0.000000f;
+    data->d[25] = 0.000000f;
+    data->d[26] = 0.000000f;
+    data->d[27] = 0.000000f;
+    data->d[28] = 0.000000f;
+    *(result->MinResult()) = 0.000035f;
+} // LoadVariation2
+
+inline float Evaluate(FireStarterData data, float n)
+{
+    n = data.d[0] *= n;
+    n = data.d[1] *= n;
+    n = data.d[2] *= n;
+    n += data.d[3];
+    n = data.d[4] += n;
+    n = data.d[5] *= n;
+    n = data.d[6] *= n;
+    n *= data.d[7];
+    n = data.d[6] *= n;
+    n = data.d[8] += n;
+    n *= data.d[8];
+    n *= data.d[9];
+    n += data.d[10];
+    n *= data.d[0];
+    n += data.d[11];
+    n *= data.d[12];
+    n *= data.d[13];
+    n = data.d[1] *= n;
+    n = data.d[14] += n;
+    n += data.d[14];
+    n = data.d[15] *= n;
+    n += data.d[1];
+    n *= data.d[16];
+    n = data.d[2] *= n;
+    n *= data.d[4];
+    n *= data.d[17];
+    n = data.d[18] += n;
+    n *= data.d[18];
+    n += data.d[5];
+    n *= data.d[15];
+    n += data.d[2];
+    n *= data.d[6];
+    return isfinite(n) ? n : 0.0f;
+} // Evaluate
+// END //
+
+//inline float TestEvaluate(const FireStarterData& data, const float target[FIRESTARTER_SAMPLES], const float theta[FIRESTARTER_SAMPLES])
+inline float TestEvaluate(const FireStarterData& data, const float target[], const float theta[])
+{
+    float result = 0.0f;
+    for (int i = 0; i < FIRESTARTER_SAMPLES; i++)
+        result = fmaxf(fabsf(Evaluate(data, theta[i]) - target[i]), result);
+    return result;
+} // TestEvaluate
+
+inline float TestPrecision(const FireStarterData& data, unsigned int variation, unsigned int precision)
+{
+    float result = 0.0f;
+    float precisionStep = (TARGET_MAX - TARGET_MIN) / (precision - 1);
+    for (int i = 0; i < precision; i++) {
+        float theta = TARGET_MIN + i * precisionStep;
+        result = fmaxf(fabsf(Evaluate(data, theta) - Target(theta, variation)), result);
+    }
+    return result;
+} // TestPrecision
+
+GPU_GLOBAL void Optimizer(const FireStarterSettings settings, FireStarterPopulation* newResults, FireStarterPopulation* oldResults, const unsigned int v, const unsigned int firstMember, const unsigned int lastMember, const unsigned int dataSize, const unsigned long long optimizationSeed, const unsigned int initData)
+{
+    unsigned int member = firstMember + blockDim.x * blockIdx.x + threadIdx.x;
+    if (member >= lastMember)
+        return;
+
+    // Precalculate the target theta values.
+    float theta[FIRESTARTER_SAMPLES];
+    float sampleStep = (TARGET_MAX - TARGET_MIN) / (FIRESTARTER_SAMPLES - 1);
+    for (int i = 0; i < FIRESTARTER_SAMPLES; i++)
+        theta[i] = TARGET_MIN + i * sampleStep;
+
+    // Evolve the program data for each variation.
+    unsigned long long seed = optimizationSeed + SEED10(v) + SEED11(member); // Unique seed for the generation/variation/member
+    FireStarterData data;
+    float oldResult;
+    bool evolved = false;
+
+    if (initData) {
+        // The first generation is initalized with random numbers.
+        for (unsigned int i = 0; i < dataSize; i++)
+            data.d[i] = RANDOMFACTOR(seed) * settings.m_startScale;
+        for (unsigned int i = dataSize; i < FIRESTARTER_REGISTERS; i++)
+            data.d[i] = 0.0f;   // Clear the unused data.
+        oldResult = settings.m_startResult;
+        evolved = true;
+    } else {
+        // Later generations randomize a single instruction if they were copied.
+        data = *oldResults->Data(member, v);
+        oldResult = *oldResults->MinResult(member, v);
+        unsigned int age = *oldResults->Index(member, v);
+        if (age > 1) {
+            unsigned int d = RANDOMMOD(seed, dataSize);
+            data.d[d] += RANDOMFACTOR(seed) * settings.m_startScale * (age - 1);
+            oldResult = settings.m_startResult;
+            evolved = true;
+        }
+    }
+
+    // Precalculate the target sample values.
+    float target[FIRESTARTER_SAMPLES];
+    for (int i = 0; i < FIRESTARTER_SAMPLES; i++)
+        target[i] = Target(theta[i], v);
+
+    // Find the initial result
+    float result = TestEvaluate(data, target, theta);
+    float evolutionScale = evolved ? settings.m_startScale : settings.m_scale * result;
+
+    // Iterate to evolve the data.
+    for (unsigned int p = 0; p < settings.m_iterations; p++) {
+        unsigned int d = RANDOMMOD(seed, dataSize);
+        float oldData = data.d[d];
+        data.d[d] = oldData + evolutionScale * RANDOMFACTOR(seed);
+        float curResult = TestEvaluate(data, target, theta);
+        if (curResult <= result) {
+            result = curResult;
+            evolutionScale = settings.m_scale * result;
+            evolved = true;
+        } else
+            data.d[d] = oldData;
+    }
+
+    // Calculate a more accurate estimate of the result.
+    if (settings.m_precision) {
+        if (evolved)
+            result = fmaxf(result, TestPrecision(data, v, settings.m_precision));
+        else
+            result = oldResult;
+    }
+
+    // If the result was better, save the results.
+    if (initData || (result < oldResult)) {
+        *newResults->Data(member, v) = data;
+        *newResults->MinResult(member, v) = result;
+        *newResults->Index(member, v) = 0;
+        *newResults->Debug(member, v) = initData ? 1 : *oldResults->Debug(member, v) + 1;
+    } else {
+        // If the result was worse, copy a result from among the previous generation's results.
+        unsigned int bestCandidate = member;
+        float bestResult = result;
+
+        // The genetic part of genetic programming and a major optimization:
+        // Copy the best data from among a random set of candidates.
+        for (int i = 0; i < settings.m_candidates; i++) {
+            unsigned int candidate = RANDOMMOD(seed, settings.m_population);
+            if (*oldResults->Index(candidate, v) <= 1) {   // Only select evolving members
+                float curResult = *oldResults->MinResult(candidate, v);
+                if (curResult <= bestResult) {
+                    bestResult = curResult;
+                    bestCandidate = candidate;
+                }
+            }
+        }
+
+        // Switch to the selected member's data and results.
+        if (bestCandidate == member) {
+            // Note: result will be larger than oldResult
+            *newResults->Data(member, v) = data;
+            *newResults->MinResult(member, v) = result;
+            *newResults->Index(member, v) = 1;
+        } else {
+            *newResults->Data(member, v) = *oldResults->Data(bestCandidate, v);
+            *newResults->MinResult(member, v) = *oldResults->MinResult(bestCandidate, v);
+            *newResults->Index(member, v) = MAX(*oldResults->Index(member, v), 1) + 1;
+        }
+        *newResults->Debug(member, v) = *oldResults->Debug(bestCandidate, v);
+    }
+} // Optimizer
---
+++ Distribution/Logs/2024-01-24_18-38-26_FireStarter_Solution.h
@@ -0,0 +1,193 @@
+// Run date: 01/24/24 18:38:26 Pacific Standard Time
+// Run duration = 28292.065595 seconds
+// Run generation = 85
+// Run evolution = 11
+// Run max result = 0.00003541
+// Run variations = 3
+// Run samples = 15
+// Run instructions = 32
+// Run registers = 29
+// Run opcodes = 2
+// Run patternOpcodes = 4
+
+// Run targetMin = 0.000000f
+// Run targetMax = 6.283185f
+// Run evolveTarget = 0.000001f
+
+// Run mode = FIRESTARTER_EVOLVE
+// Run evolveSeed = 0
+// Run optimizeSeed = 0
+// Run tests = 256
+// Run streams = 8
+// Run units = 1
+// Run states = 1
+// Run generations = 128
+// Run population = 278528
+// Run iterations = 64
+// Run passes = 500
+// Run precision = 0
+// Run candidates = 16
+// Run attempts = 0
+// Run optimize = 8
+
+// Run scale = 0.100000f
+// Run startScale = 2.000000f
+// Run startResult = 10.000000f
+
+#define SOLUTION_VARIATIONS 3
+
+#ifndef __CUDACC__
+#include <cmath>
+#endif
+
+#define SOLUTION_PI 3.14159265f
+#define SOLUTION_MIN 0.0f
+#define SOLUTION_MAX (2.0f * 3.14159265f)
+
+inline float SolutionTarget(float n, unsigned int variation)
+{
+    switch (variation) {
+    default:
+    case 0:
+        return sinf(n);
+    case 1:
+        return sinf((n + 0.4f) * 0.9f) - n * 0.2f + 0.5f;
+    case 2:
+        return sinf(n * 1.2f) + n * 0.2f;
+    case 3:
+        return fabsf(fmodf(fabsf(n - SOLUTION_PI * 0.5f), SOLUTION_PI * 2.0f) - SOLUTION_PI) - SOLUTION_PI * 0.5f;
+    }
+} // SolutionTarget
+
+// Solution0 result = 0.00000432
+inline float Solution0(float n)
+{
+    float r0, r1, r2, r3, r4, r5, r6;
+
+    r0 = n *= 1.11894989f;
+    r1 = n *= 0.58015043f;
+    r2 = n *= 0.16347763f;
+    n += -1.81166315f;
+    r3 = n += 0.33265868f;
+    r4 = n *= 3.63555312f;
+    r5 = n *= -0.07204774f;
+    n *= -2.59188819f;
+    n = r5 *= n;
+    r6 = n += 0.00710084f;
+    n *= r6;
+    n *= -1.22958100f;
+    n += 1.52118921f;
+    n *= r0;
+    n += -1.09127760f;
+    n *= -0.10717368f;
+    n *= 0.73010075f;
+    n = r1 *= n;
+    r0 = n += 0.64889091f;
+    n += r0;
+    r0 = n *= -0.69779390f;
+    n += r1;
+    n *= -0.20722881f;
+    n = r2 *= n;
+    n *= r3;
+    n *= -18.11231995f;
+    r3 = n += 2.31883836f;
+    n *= r3;
+    n += r4;
+    n *= r0;
+    n += r2;
+    n *= r5;
+    return n;
+} // Solution0
+
+// Solution1 result = 0.00001127
+inline float Solution1(float n)
+{
+    float r0, r1, r2, r3, r4, r5, r6;
+
+    r0 = n *= 0.62871140f;
+    r1 = n *= -0.21725912f;
+    r2 = n *= 0.65785664f;
+    n += 0.50120109f;
+    r3 = n += 0.83071971f;
+    r4 = n *= -0.43004984f;
+    r5 = n *= 0.19471315f;
+    n *= 99.12127686f;
+    n = r5 *= n;
+    r6 = n += 2.79219818f;
+    n *= r6;
+    n *= 0.01025918f;
+    n += -2.45752501f;
+    n *= r0;
+    n += 1.62043071f;
+    n *= -1.51200104f;
+    n *= -0.87473017f;
+    n = r1 *= n;
+    r0 = n += -1.28846240f;
+    n += r0;
+    r0 = n *= -1.14041460f;
+    n += r1;
+    n *= -0.62139809f;
+    n = r2 *= n;
+    n *= r3;
+    n *= 0.15581918f;
+    r3 = n += 0.89889133f;
+    n *= r3;
+    n += r4;
+    n *= r0;
+    n += r2;
+    n *= r5;
+    return n;
+} // Solution1
+
+// Solution2 result = 0.00003541
+inline float Solution2(float n)
+{
+    float r0, r1, r2, r3, r4, r5, r6;
+
+    r0 = n *= 0.62745064f;
+    r1 = n *= 0.33181465f;
+    r2 = n *= -1.04751110f;
+    n += 0.68836743f;
+    r3 = n += 1.84087205f;
+    r4 = n *= -0.89398944f;
+    r5 = n *= 0.14885330f;
+    n *= 1.97461998f;
+    n = r5 *= n;
+    r6 = n += -0.04110055f;
+    n *= r6;
+    n *= 17.46567345f;
+    n += -7.94467878f;
+    n *= r0;
+    n += 3.96241164f;
+    n *= -0.27707642f;
+    n *= 0.52564740f;
+    n = r1 *= n;
+    r0 = n += -1.63906419f;
+    n += r0;
+    r0 = n *= -2.50968122f;
+    n += r1;
+    n *= 1.64876831f;
+    n = r2 *= n;
+    n *= r3;
+    n *= -0.04114080f;
+    r3 = n += 1.50369453f;
+    n *= r3;
+    n += r4;
+    n *= r0;
+    n += r2;
+    n *= r5;
+    return n;
+} // Solution2
+
+inline float Solution(float n, unsigned int variation)
+{
+    switch (variation) {
+    case 0:
+        return Solution0(n);
+    case 1:
+        return Solution1(n);
+    case 2:
+        return Solution2(n);
+    }
+    return 0.0f;
+} // Solution
