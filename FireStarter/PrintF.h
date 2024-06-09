8bfa075f41d2937c446f3ad4f219a8f2eb8675d2
Mark Granger <grangerfx@gmail.com>
2024-06-09 15:17:30 -0700

    FIxed build warnings.

--- FireStarter/PrintF.h
+++ FireStarter/PrintF.h
@@ -32,3 +32,29 @@ inline int printf2(const char *format, ...)
     std::cerr << zc.data();
     return ret;
 } // printf2
+
+// Test the input but don't output anything.
+inline int noprintf(const char* format, ...)
+{
+    // initialize use of the variable argument array
+    va_list vaArgs;
+    va_start(vaArgs, format);
+
+    // reliably acquire the size from a copy of
+    // the variable argument array
+    // and a functionally reliable call
+    // to mock the formatting
+    va_list vaCopy;
+    va_copy(vaCopy, vaArgs);
+    const int iLen = std::vsnprintf(NULL, 0, format, vaCopy);
+    va_end(vaCopy);
+
+    // return a formatted string without
+    // risking memory mismanagement
+    // and without assuming any compiler
+    // or platform specific behavior
+    std::vector<char> zc(iLen + 1);
+    int ret = std::vsnprintf(zc.data(), zc.size(), format, vaArgs);
+    va_end(vaArgs);
+    return ret;
+} // noprintf
