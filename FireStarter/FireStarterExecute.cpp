767d05efd151e2abe5a7b420ef589fcb44e6556a
Mark Granger <grangerfx@gmail.com>
2024-06-22 08:50:22 -0700

    Old data copy. Index 8 first 500.

--- FireStarter/FireStarterExecute.cpp
+++ FireStarter/FireStarterExecute.cpp
@@ -124,15 +124,15 @@ float FireStarterExecute::OptimizeGenerations(FireStarterState& state, unsigned 
         optimizationPass++;
 #if 1
         // Note: DEBUG!
-        if ((checksumIndex == 9) && (p == 0)) {
+        if ((checksumIndex == 8) && (p == 0)) {
             checkCUDAErrors(cudaMemcpyAsync(m_hostPopulation, newResults, m_populationSize, cudaMemcpyDeviceToHost, stream));
             context->Synchronize();
 
             uint64_t checksum = Checksum(m_hostPopulation, m_hostPopulation->PopulationSize(settings));
             std::string checksumString = Format("Test: %4d  ID: %4d  Pass:%4d  Variation: %d  Index: %4d  Checksum: %.16llX\n", state.m_test, state.m_id, optimizationPass, variation, checksumIndex, checksum);
 
-            unsigned int i = 79;
-//            for (unsigned int i = 0; i < 500; i++)
+//            unsigned int i = 79;
+            for (unsigned int i = 0; i < 500; i++)
             {
                 FireStarterResult* result = m_hostPopulation->Result(settings, i, variation);
                 for (unsigned int j = 0; j < settings.m_registers; j++)
