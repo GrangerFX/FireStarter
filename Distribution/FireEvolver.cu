87a270ca9414cde40051525f636b82721ccbf09a
Mark Granger <grangerfx@gmail.com>
2024-09-15 18:42:56 -0700

    Evolve modification for test.

--- Distribution/FireEvolver.cu
+++ Distribution/FireEvolver.cu
@@ -69,7 +69,7 @@ GPU_GLOBAL void Evolver(FireStarterPopulation* newResults, const FireStarterPopu
     } else {
         evolveAge1 = oldResults->EvolveAge1(member);
         evolveAge2 = oldResults->EvolveAge2(member) ;
-        if (evolveAge1 >= MAX(evolveAge2, 1024)) {
+        if (evolveAge1 >= MAX(evolveAge2, 256)) {
             evolveAge1 = 0;
             evolveAge2 = 0;
             memberResult = FIRESTARTER_START_RESULT;
