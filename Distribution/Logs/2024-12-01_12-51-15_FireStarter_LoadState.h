ec12f04f26ae5997f0ee528d9527fd5cd4f1e243
Mark Granger <grangerfx@gmail.com>
2024-12-01 14:16:01 -0800

    Seed 2: Average = 2.5s

---
+++ Distribution/Logs/2024-12-01_12-51-15_FireStarter_LoadState.h
@@ -0,0 +1,168 @@
+#pragma once
+#include "FireStarterState.h"
+
+// Run date: 12/01/24 12:51:15 Pacific Standard Time
+// Run duration = 650.054743 seconds
+// Run generation = 9
+// Run evolution = 0
+// Run max result = 0.00000028
+// Run variations = 1
+// Run instructions = 32
+// Run registers = 30
+// Run opcodes = 2
+// Run patternOpcodes = 4
+
+// Run targetMin = 0.000000f
+// Run targetMax = 6.283185f
+// Run target = 0.000001f
+
+// Run mode = FIRESTARTER_OPTIMIZE
+// Run evolveSeed = 2
+// Run optimizeSeed = 2
+// Run tests = 256
+// Run streams = 1
+// Run units = 1
+// Run states = 1
+// Run generations = 0
+// Run population = 65536
+// Run passes = 384
+// Run attempts = 0
+// Run optimize = 1
+
+// Run scale = 0.300000f
+// Run startScale = 2.500000f
+// Run startResult = 10.000000f
+
+inline void LoadSettings(FireStarterSettings& settings)
+{
+    settings.m_variations = 1;
+    settings.m_instructions = 32;
+    settings.m_registers = 30;
+    settings.m_opcodes = 2;
+    settings.m_patternOpcodes = 4;
+
+    settings.m_targetMin = 0.000000f;
+    settings.m_targetMax = 6.283185f;
+    settings.m_target = 0.000001f;
+
+    settings.m_mode = FIRESTARTER_OPTIMIZE;
+    settings.m_evolveSeed = 2;
+    settings.m_optimizeSeed = 2;
+    settings.m_tests = 256;
+    settings.m_streams = 1;
+    settings.m_units = 1;
+    settings.m_states = 1;
+    settings.m_generations = 0;
+    settings.m_population = 65536;
+    settings.m_passes = 384;
+    settings.m_attempts = 0;
+    settings.m_optimize = 1;
+
+    settings.m_scale = 0.300000f;
+    settings.m_startScale = 2.500000f;
+    settings.m_startResult = 10.000000f;
+} // LoadSettings
+
+// Variation: 0  result = 0.00000028
+inline void LoadVariation0(FireStarterResult* result)
+{
+    FireStarterData *data = result->Data();
+    data->d[0] = 0.37835112f;
+    data->d[1] = -1.18862510f;
+    data->d[2] = 0.06539704f;
+    data->d[3] = 2.53599429f;
+    data->d[4] = -0.32132176f;
+    data->d[5] = -2.22227788f;
+    data->d[6] = 1.77003491f;
+    data->d[7] = 0.09913540f;
+    data->d[8] = -0.65087658f;
+    data->d[9] = -0.72982442f;
+    data->d[10] = -2.17393947f;
+    data->d[11] = -1.98183501f;
+    data->d[12] = 1.78404403f;
+    data->d[13] = -1.88722301f;
+    data->d[14] = 2.48339152f;
+    data->d[15] = 0.91666758f;
+    data->d[16] = -0.59029430f;
+    data->d[17] = 4.81360054f;
+    data->d[18] = 1.60627270f;
+    data->d[19] = -1.05208898f;
+    data->d[20] = 0.38232362f;
+    data->d[21] = -1.86977100f;
+    data->d[22] = 1.17144608f;
+    data->d[23] = -2.49430943f;
+    data->d[24] = 0.36812699f;
+    data->d[25] = -1.58695912f;
+    data->d[26] = -1.92722309f;
+    data->d[27] = -1.71094418f;
+    data->d[28] = 1.66394687f;
+    data->d[29] = 0.22020580f;
+    *(result->MinResult()) = 0.00000028f;
+} // LoadVariation0
+
+inline void LoadResult(FireStarterState& state)
+{
+    LoadVariation0(state.Result(0));
+    state.m_maxResult = 0.00000028f;
+} // LoadResult
+
+inline void LoadProgram(FireStarterProgram& program)
+{
+    FireStarterInstructions* instructions = program.EvolvedInstructions();
+    instructions->SetOperation(0, 0, 0);
+    instructions->SetOperation(1, 1, 1);
+    instructions->SetOperation(2, 0, 2);
+    instructions->SetOperation(3, 0, 1);
+    instructions->SetOperation(4, 0, 3);
+    instructions->SetOperation(5, 1, 4);
+    instructions->SetOperation(6, 1, 4);
+    instructions->SetOperation(7, 1, 5);
+    instructions->SetOperation(8, 0, 1);
+    instructions->SetOperation(9, 1, 6);
+    instructions->SetOperation(10, 0, 7);
+    instructions->SetOperation(11, 1, 3);
+    instructions->SetOperation(12, 0, 8);
+    instructions->SetOperation(13, 1, 6);
+    instructions->SetOperation(14, 0, 9);
+    instructions->SetOperation(15, 1, 10);
+    instructions->SetOperation(16, 1, 9);
+    instructions->SetOperation(17, 0, 11);
+    instructions->SetOperation(18, 1, 12);
+    instructions->SetOperation(19, 0, 13);
+    instructions->SetOperation(20, 1, 14);
+    instructions->SetOperation(21, 0, 6);
+    instructions->SetOperation(22, 0, 1);
+    instructions->SetOperation(23, 1, 9);
+    instructions->SetOperation(24, 1, 9);
+    instructions->SetOperation(25, 0, 15);
+    instructions->SetOperation(26, 0, 16);
+    instructions->SetOperation(27, 0, 17);
+    instructions->SetOperation(28, 0, 18);
+    instructions->SetOperation(29, 1, 1);
+    instructions->SetOperation(30, 0, 19);
+    instructions->SetOperation(31, 0, 2);
+    program.OptimizeRegisters();
+} // LoadProgram
+
+inline void LoadState(FireStarterState& state)
+{
+    FireStarterSettings settings;
+
+    LoadSettings(settings);
+    state.InitState(settings);
+    LoadResult(state);
+    LoadProgram(state.m_program);
+    state.LoadCodeFromProgram();
+    state.m_generation = 9;
+    state.m_evolution = 0;
+    state.m_index = 0;
+    state.m_copy_index = 0;
+    state.m_id = 0;
+    state.m_test = 255;
+    state.m_seed = 4172340983833127324;
+    state.m_optimize_pass = 0;
+    state.m_oldResult = 10.00000000f;
+    state.m_maxResult = 0.00000028f;
+    state.m_evolveWeight = 0.000000f;
+    state.m_optimizeValid = true;
+} // LoadState
---
+++ Distribution/Logs/2024-12-01_12-51-15_FireStarter_Solution.h
@@ -0,0 +1,95 @@
+// Run date: 12/01/24 12:51:15 Pacific Standard Time
+// Run duration = 650.055984 seconds
+// Run generation = 9
+// Run evolution = 0
+// Run max result = 0.00000028
+// Run variations = 1
+// Run instructions = 32
+// Run registers = 30
+// Run opcodes = 2
+// Run patternOpcodes = 4
+
+// Run targetMin = 0.000000f
+// Run targetMax = 6.283185f
+// Run target = 0.000001f
+
+// Run mode = FIRESTARTER_OPTIMIZE
+// Run evolveSeed = 2
+// Run optimizeSeed = 2
+// Run tests = 256
+// Run streams = 1
+// Run units = 1
+// Run states = 1
+// Run generations = 0
+// Run population = 65536
+// Run passes = 384
+// Run attempts = 0
+// Run optimize = 1
+
+// Run scale = 0.300000f
+// Run startScale = 2.500000f
+// Run startResult = 10.000000f
+
+#define SOLUTION_VARIATIONS 1
+
+#ifndef __CUDACC__
+#include <cmath>
+#endif
+
+#define SOLUTION_PI 3.14159265f
+#define SOLUTION_MIN 0.0f
+#define SOLUTION_MAX (2.0f * 3.14159265f)
+
+inline float SolutionTarget(float n, unsigned int variation = 0)
+{
+    switch (variation) {
+        default:
+        case 0:
+            return sinf(n);
+        case 1:
+            return sinf((n + 0.4f) * 0.9f) - n * 0.2f + 0.5f;
+        case 2:
+            return sinf(n * 1.2f) + n * 0.2f;
+        case 3:
+            return fabsf(fmodf(fabsf(n - SOLUTION_PI * 0.5f), SOLUTION_PI * 2.0f) - SOLUTION_PI) - SOLUTION_PI * 0.5f;
+    }
+} // SolutionTarget
+
+inline float Solution(float n)
+{
+    float r0, r1, r2, r3;
+
+    n *= 0.37835112f;
+    r0 = n += -1.18862510f;
+    r1 = n *= 0.06539704f;
+    n = r0 *= n;
+    r2 = n *= 2.53599429f;
+    r3 = n += -0.32132176f;
+    n += r3;
+    n += -2.22227788f;
+    n = r0 *= n;
+    r3 = n += 1.77003491f;
+    n *= 0.09913540f;
+    n += r2;
+    n *= -0.65087658f;
+    n = r3 += n;
+    r2 = n *= -0.72982442f;
+    n += -2.17393947f;
+    n = r2 += n;
+    n *= -1.98183501f;
+    n += 1.78404403f;
+    n *= -1.88722301f;
+    n += 2.48339152f;
+    n *= r3;
+    n = r0 *= n;
+    n = r2 += n;
+    n += r2;
+    n *= 0.91666758f;
+    n *= -0.59029430f;
+    n *= 4.81360054f;
+    n *= 1.60627270f;
+    n += r0;
+    n *= -1.05208898f;
+    n *= r1;
+    return n;
+} // Solution
