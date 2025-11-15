d607e690b5a4194d30087351f856e66834148c6d
Mark Granger <mark.granger@otoy.com>
2025-11-15 12:47:53 -0800

    Test of new day trade mode.

--- Distribution/Logs/2025-11-15_11-47-11_FireMoneyOptimizer.cu
+++ Distribution/Logs/2025-11-15_11-47-11_FireMoneyOptimizer.cu
@@ -6,6 +6,38 @@
 inline float MoneyCompiledEvaluate(FireStarterData& data, float n)
 {
 // EVALUATE //
+    n += data[0];
+    data[1] = n;
+    data[2] = n;
+    data[3] = n;
+    n *= data[4];
+    n *= data[5];
+    data[6] = n;
+    data[7] = n;
+    data[8] = n;
+    n += data[9];
+    n *= data[10];
+    n *= data[4];
+    n *= data[1];
+    n += data[11];
+    data[0] = n;
+    data[12] = n;
+    data[4] = n;
+    n += data[0];
+    data[1] = n;
+    data[13] = n;
+    n += data[7];
+    n *= data[1];
+    n += data[14];
+    n += data[15];
+    data[4] = n;
+    n *= data[2];
+    n += data[10];
+    n += data[16];
+    n += data[17];
+    n *= data[18];
+    n *= data[13];
+    n += data[2];
 // END //
     return n;
 } // MoneyCompiledEvaluate
--- FireStarter/FireStarterShow.cpp
+++ FireStarter/FireStarterShow.cpp
@@ -95,19 +95,35 @@ void FireStarterShow::FireShow(const FireStarterState& state, const MoneyMakerSt
                 int y = (int)(height * 0.75f - (curValue - minValue) * yScale);
                 if ((y >= 0) && ((unsigned int)y < height)) {
                     if (i >= warmup) {
-                        if (!holding && (results[i] > 1.0f)) {
-                            unsigned int j = i + 1;
-                            while (j < numDays) {
-                                if (results[j] < -1.0f)
-                                    break;
-                                j++;
-                            }
-                            if (j == numDays)
-                                j--;
-                            good = stock[j] >= stock[i];
-                            holding = true;
-                        } else if (holding && (results[i] < -1.0f))
-                            holding = false;
+                        if (settings.m_daytrade) {
+                            if (!holding && (results[i] >= 1.0f)) {
+                                unsigned int j = i + 1;
+                                while (j < numDays) {
+                                    if (results[j] < 1.0f)
+                                        break;
+                                    j++;
+                                }
+                                if (j == numDays)
+                                    j--;
+                                good = stock[j] >= stock[i];
+                                holding = true;
+                            } else if (holding && (results[i] < 1.0f))
+                                holding = false;
+                        } else {
+                            if (!holding && (results[i] > 1.0f)) {
+                                unsigned int j = i + 1;
+                                while (j < numDays) {
+                                    if (results[j] < -1.0f)
+                                        break;
+                                    j++;
+                                }
+                                if (j == numDays)
+                                    j--;
+                                good = stock[j] >= stock[i];
+                                holding = true;
+                            } else if (holding && (results[i] < -1.0f))
+                                holding = false;
+                        }
                     }
 
                     uchar4& pixel(pixels[y * width + x]);
