e6c0af15783d473b1c7b106fb3fbc8444686d178
Mark Granger <mark.granger@otoy.com>
2025-11-24 10:57:27 -0800

    Increased the variation to 180 to offset the 60 trading days. Much better results but validation needed.

--- Distribution/FireStarter_MoneyMakerSolution.h
+++ Distribution/FireStarter_MoneyMakerSolution.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "MoneyMakerStocks.h"
 
-// Run date: 11/23/25 21:11:52 Pacific Standard Time
-// Run duration = 775.579242 seconds
+// Run date: 11/24/25 09:31:04 Pacific Standard Time
+// Run duration = 777.567814 seconds
 // Run test = 0
 // Run generation = 2
 // Run evolution = 0
-// Run max result = 0.84650844
+// Run max result = 0.86888015
 
 // Run variations = 1
 // Run instructions = 32
@@ -15,8 +15,8 @@
 
 // Run stocks = 8
 // Run sessions = 64
-// Run history = 197
-// Run variation = 120
+// Run history = 257
+// Run variation = 180
 // Run warmup = 16
 // Run trading = 60
 // Run funds = 1000.000000f
@@ -45,61 +45,62 @@
 inline float MoneyMakerSolution(MoneyMakerStock& stock)
 {
     float n = 0.0f;
-    float r0 = 0.74827874f;
-    float r1 = 0.63139039f;
-    float r2 = -1.14261663f;
-    float r3 = 2.14875841f;
-    float r4 = 1.04845095f;
-    float r5 = -1.16689205f;
-    float r6 = -0.66921085f;
-    float r7 = 0.07905826f;
-    float r8 = 1.33671665f;
-    float r9 = -1.13860416f;
-    float r10 = -1.10368896f;
-    float r11 = 1.71962452f;
-    float r12 = -0.44509888f;
-    float r13 = -0.08367326f;
-    float r14 = 1.96327150f;
-    float r15 = -1.64541709f;
-    float r16 = -0.42167974f;
-    float r17 = 1.18792462f;
-    float r18 = 1.76121831f;
-    float r19 = -1.71738124f;
+    float r0 = -1.23082399f;
+    float r1 = 1.60799551f;
+    float r2 = 0.30947921f;
+    float r3 = -3.94848347f;
+    float r4 = -0.04392180f;
+    float r5 = -0.55310869f;
+    float r6 = 1.89698792f;
+    float r7 = 0.21880357f;
+    float r8 = 1.20645881f;
+    float r9 = -1.88717222f;
+    float r10 = 0.66342527f;
+    float r11 = -0.22632650f;
+    float r12 = 1.03582168f;
+    float r13 = -0.73533946f;
+    float r14 = 2.45333695f;
+    float r15 = 2.17743874f;
+    float r16 = 1.28136349f;
+    float r17 = 0.49313217f;
+    float r18 = 0.32965958f;
+    float r19 = 2.36777568f;
+    float r20 = -1.96500075f;
 
     for (unsigned int d = 0; d < stock.numDays; d++) {
         n = stock[d];
-        r0 = n;
-        n *= r1;
+        n *= r0;
+        n += r1;
         r2 = n;
-        r3 = n;
-        r4 = n;
+        n *= r3;
+        n += r4;
         r5 = n;
         n += r6;
         n *= r7;
-        n *= r8;
-        n += r7;
-        n *= r9;
+        r8 = n;
+        r7 = n;
+        n += r9;
         r10 = n;
         n *= r11;
-        n += r4;
-        r12 = n;
-        n += r8;
+        n *= r12;
+        n += r2;
+        r10 = n;
         r13 = n;
-        n *= r10;
-        n += r14;
-        n += r15;
-        n *= r16;
-        r7 = n;
-        n *= r4;
-        n += r17;
-        n *= r18;
-        n *= r19;
-        n += r4;
-        n += r13;
-        n *= r19;
-        n += r10;
+        n += r5;
         r14 = n;
-        n *= r7;
+        r15 = n;
+        r3 = n;
+        n += r16;
+        n *= r17;
+        n += r18;
+        r6 = n;
+        n *= r10;
+        r14 = n;
+        n *= r5;
+        n *= r12;
+        n *= r19;
+        n *= r20;
+        r14 = n;
     }
     return n;
 } // MoneyMakerSolution
