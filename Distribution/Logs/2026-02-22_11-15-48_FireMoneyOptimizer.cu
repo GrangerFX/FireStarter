5646efb2875dc2d9e3bf78d132c07727c9f5ebe2
Mark Granger <mark.granger@otoy.com>
2026-02-22 11:39:25 -0800

    More validation experiments.

--- Distribution/Logs/2026-02-22_11-15-48_FireMoneyOptimizer.cu
+++ Distribution/Logs/2026-02-22_11-15-48_FireMoneyOptimizer.cu
@@ -6,6 +6,38 @@
 inline float MoneyCompiledEvaluate(FireStarterData& data, float n)
 {
 // EVALUATE //
+    data[0] = n;
+    data[1] = n;
+    n += data[2];
+    data[3] = n;
+    data[4] = n;
+    n = data[5];
+    n += data[6];
+    n = data[7];
+    n = data[8];
+    n = data[9];
+    n += data[10];
+    data[11] = n;
+    n += data[10];
+    n = data[12];
+    n = data[13];
+    n *= data[7];
+    n += data[14];
+    n *= data[5];
+    data[15] = n;
+    data[7] = n;
+    n += data[5];
+    n += data[16];
+    n = data[17];
+    data[18] = n;
+    n *= data[19];
+    data[2] = n;
+    n *= data[10];
+    n += data[1];
+    data[10] = n;
+    n *= data[11];
+    n *= data[5];
+    n *= data[20];
 // END //
     return n;
 } // MoneyCompiledEvaluate
