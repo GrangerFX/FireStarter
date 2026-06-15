ba95c765357f0d98b94360105ed3c10b8bb07635
Mark Granger <mark.granger@otoy.com>
2026-06-15 10:25:28 -0700

    The CUDA device properties are now determined for each device during static intitialization.

--- FireStarter/CUDAContext.h
+++ FireStarter/CUDAContext.h
@@ -72,6 +72,7 @@ private:
     CUstream m_stream = nullptr;
     inline static int m_CUDA_devices = 0;
     inline static bool m_initialized = false;
+    inline static std::vector<cudaDeviceProp> m_deviceProperties;
 
     // Initialize CUDA only once per process.
     static inline void CUDAInitialize(void)
@@ -84,8 +85,32 @@ private:
 
             // Find the number of CUDA devices.
             checkCUDAErrors(cuDeviceGetCount(&m_CUDA_devices));
-            if (m_CUDA_devices < 0)
+            if (m_CUDA_devices <= 0)
                 m_CUDA_devices = 0;
+            else {
+                for (int i = 0; i < m_CUDA_devices; i++) {
+                    // Get information about each CUDA device in the computer.
+                    CUdevice device;
+                    checkCUDAErrors(cuDeviceGet(&device, i));
+                    cudaDeviceProp deviceProperties;
+                    checkCUDAErrors(cudaGetDeviceProperties(&deviceProperties, device));
+                    m_deviceProperties.push_back(deviceProperties);
+#if 1
+                    // Get some information about the device.
+                    // A block is the kernal threads that run together the size as specified when the kernal is launched.
+                    // Note: My original idea was to find out how many kernel threads or warps I should send to the GPU in order to keep it fully loaded.
+                    // This actually makes no sense since the number of active threads being executed per SM is based on the registers and shared memory
+                    // they use up to the maximum per multiprocessor and/or block. These are then executed until they are all complete but not necessarilly
+                    // at the same time. Use the CUDA occupancy API to determine the number of kernel threads can run on a SM.
+                    int multiProcessorCount = deviceProperties.multiProcessorCount;                   // Number of multiprocessors on device: 128 (matches online documentation for the RTX 4090)
+                    int maxBlocksPerMultiProcessor = deviceProperties.maxBlocksPerMultiProcessor;     // Maximum number of resident blocks per multiprocessor: 24
+                    int maxThreadsPerMultiProcessor = deviceProperties.maxThreadsPerMultiProcessor;   // Maximum resident threads per multiprocessor: 1536
+                    int maxThreadsPerBlock = deviceProperties.maxThreadsPerBlock;                     // Maximum number of threads per block: 1024
+                    int warpSize = deviceProperties.warpSize;                                         // Warp size in threads: 32
+                    // Missing: Warps per SM: 64 for all current CUDA GPUs accourding to online documentation.
+#endif
+                }
+            }
         }
         CUDAContextMutex.unlock();
     } // CUDAInitialize
@@ -156,8 +181,11 @@ public:
             if (m_CUDA_devices)
                 m_CUDA_device = (int)(deviceIndex % m_CUDA_devices);
             else
-                m_CUDA_device = 0;
-
+#if 1
+                m_CUDA_device = 0;                  // First CUDA device
+#else
+                m_CUDA_device = m_CUDA_devices - 1; // Last CUDA device
+#endif
             // Create a context and stream on the device.
             checkCUDAErrors(cuDeviceGet(&m_device, m_CUDA_device));
 
@@ -168,23 +196,6 @@ public:
     //      checkCUDAErrors(cuCtxCreate(&m_context, CU_CTX_SCHED_SPIN, m_device));
 
             checkCUDAErrors(cudaStreamCreateWithPriority(&m_stream, cudaStreamDefault, priority));
-
-#if 1
-            // Get some information about the device.
-            // A block is the kernal threads that run together the size as specified when the kernal is launched.
-            // Note: My original idea was to find out how many kernel threads or warps I should send to the GPU in order to keep it fully loaded.
-            // This actually makes no sense since the number of active threads being executed per SM is based on the registers and shared memory
-            // they use up to the maximum per multiprocessor and/or block. These are then executed until they are all complete but not necessarilly
-            // at the same time. Use the CUDA occupancy API to determine the number of kernel threads can run on a SM.
-            cudaDeviceProp deviceProperties;
-            checkCUDAErrors(cudaGetDeviceProperties(&deviceProperties, m_device));
-            int multiProcessorCount = deviceProperties.multiProcessorCount;                   // Number of multiprocessors on device: 128 (matches online documentation for the RTX 4090)
-            int maxBlocksPerMultiProcessor = deviceProperties.maxBlocksPerMultiProcessor;     // Maximum number of resident blocks per multiprocessor: 24
-            int maxThreadsPerMultiProcessor = deviceProperties.maxThreadsPerMultiProcessor;   // Maximum resident threads per multiprocessor: 1536
-            int maxThreadsPerBlock = deviceProperties.maxThreadsPerBlock;                     // Maximum number of threads per block: 1024
-            int warpSize = deviceProperties.warpSize;                                         // Warp size in threads: 32
-            // Missing: Warps per SM: 64 for all current CUDA GPUs accourding to online documentation.
-#endif
         }
     } // InitContext
 
